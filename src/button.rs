94d521f29a4531bc91c2183658e0222362c537e5
lrshsl <hoeslilars@gmail.com>
2024-11-30 09:35:11 +0100

    Select tracks when not painting

--- src/button.rs
+++ src/button.rs
@@ -1,26 +1,67 @@
-use crate::{prelude::*, TrackData};
+use crate::{prelude::*, TrackId};
+
+pub(crate) struct TrackButton {
+    pub id: TrackId,
+    button: Button,
+}
+
+impl TrackButton {
+    pub const fn new(
+        id: TrackId,
+        text: &'static str,
+        rect: Rect,
+        fg_clr: Color,
+        bg_clr: Color,
+    ) -> Self {
+        Self {
+            id,
+            button: Button::new(text, rect, fg_clr, bg_clr),
+        }
+    }
+
+    pub fn draw(&self, ctx: &Context) {
+        if ctx.current_track == Some(self.id) {
+            self.button.draw_clr(self.button.fg_clr, self.button.bg_clr);
+        } else {
+            self.button.draw();
+        }
+    }
+
+    pub fn is_clicked(&self) -> bool {
+        self.button.is_clicked()
+    }
+}
 
 #[derive(Clone, Debug)]
 pub(crate) struct Button {
     text: &'static str,
-    clr: Color,
     rect: Rect,
+    fg_clr: Color,
+    bg_clr: Color,
 }
 
 impl Button {
-    pub const fn new(text: &'static str, clr: Color, rect: Rect) -> Self {
-        Self { text, clr, rect }
+    pub const fn new(text: &'static str, rect: Rect, fg_clr: Color, bg_clr: Color) -> Self {
+        Self {
+            text,
+            rect,
+            fg_clr,
+            bg_clr,
+        }
     }
 
     pub fn draw(&self) {
-        let (x, y) = self.rect.point().into();
-        let (w, h) = self.rect.size().into();
-        draw_rectangle_lines(x, y, w, h, 5.0, self.clr);
+        self.draw_clr(self.bg_clr, self.fg_clr);
+    }
+
+    pub fn draw_clr(&self, bg_clr: Color, fg_clr: Color) {
+        draw_rect(self.rect, bg_clr);
+        draw_rect_lines(self.rect, 5.0, fg_clr);
         draw_text_centered!(self.text,
-                            x + w * 0.5,
-                            y + h * 0.5,
+                            self.rect.x + self.rect.w * 0.5,
+                            self.rect.y + self.rect.h * 0.5,
                             font_size: SMALLER_FONT_SIZE,
-                            color: self.clr);
+                            color: fg_clr);
     }
 
     pub fn is_clicked(&self) -> bool {
--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,9 @@
-use macroquad::math::{vec2, Vec2};
+use crate::prelude::*;
 
 pub(crate) const W: f32 = 700.0;
 pub(crate) const H: f32 = 700.0;
 pub(crate) const AREA_SIZE: Vec2 = vec2(W, H);
-pub(crate) const RESOLUTION: Vec2 = vec2(1920.0, 1080.0);
+pub(crate) const RESOLUTION: Vec2 = vec2(3840.0, 2160.0);
 
 pub(crate) const MIN_CANVAS_SIZE: Vec2 = vec2(600.0, 400.0);
 
@@ -12,14 +12,20 @@ pub(crate) const TABLE_SIZE: Vec2 = vec2(
     vw(100.0) - 2.0 * TABLE_MARGIN.x,
     vh(100.0) - 2.0 * TABLE_MARGIN.y,
 );
+pub(crate) const TABLE_RECT: Rect = Rect {
+    x: TABLE_MARGIN.x,
+    y: TABLE_MARGIN.y,
+    w: TABLE_SIZE.x,
+    h: TABLE_SIZE.y,
+};
 
-pub(crate) const DEFAULT_FONT_SIZE: f32 = 32.0;
-pub(crate) const SMALLER_FONT_SIZE: f32 = 24.0;
+pub(crate) const DEFAULT_FONT_SIZE: f32 = 24.0;
+pub(crate) const SMALLER_FONT_SIZE: f32 = 20.0;
 
 pub(crate) const BUTTON_SIZE: Vec2 = vec2(128.0, 32.0);
 
 pub(crate) const THICK_LINES: f32 = 3.0;
-pub(crate) const THIN_LINES: f32 = 1.0;
+pub(crate) const THIN_LINES: f32 = 2.0;
 
 pub const fn vw(w: f32) -> f32 {
     w * W / 100.0
